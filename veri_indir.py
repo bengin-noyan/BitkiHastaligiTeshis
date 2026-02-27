3b2adeecf3d460f1c1aa868c3c354646dd1f7ea2
bengin-noyan <benginnyn72@gmail.com>
2026-02-27 10:31:41 +0300

    api key is invisibled

--- veri_indir.py
+++ veri_indir.py
@@ -1,7 +1,7 @@
 
 
 from roboflow import Roboflow
-rf = Roboflow(api_key="VwRlrotjxhLZc92fvGWv")
+rf = Roboflow(api_key="MY_API_KEY")
 project = rf.workspace("bengins-workspace-n2nmq").project("plant-disease-tmyq8-eav1r")
 version = project.version(1)
 dataset = version.download("yolov8")
