faeec9c9ababcc30a1ee67d1d565197d02f1d49b
bengin-noyan <benginnyn72@gmail.com>
2026-07-31 10:31:07 +0300

    chore: kullanılmayan script ve test görselleri temizlendi

--- test_et.py
+++
@@ -1,7 +0,0 @@
-from ultralytics import YOLO
-
-# Şampiyon modeli yüklüyoruz
-model = YOLO("best.pt")
-
-# Yeni hastamızı (Tomato Leaf Curl) muayene ediyoruz
-results = model.predict(source="Tomato-Leaf-Curl-1600x800.jpg",  save=True)
